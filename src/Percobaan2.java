8083878b2a7c2e329d48b6295eb73dabc457ad6e
betrindina <betrindina@gmail.com>
2026-04-20 23:46:46 +0700

    Memperbaiki no PONIRIN di commit awal dan mengisi tabel nama dan spasinya

--- src/Percobaan2.java
+++ src/Percobaan2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Percobaan2 {
     public static void main(String[] args) {
-        String data[][] = {{"ABDUL", "085646668991", "Kediri"}, {"KUSNO", "085646668992", "Trenggalek"}, {"PONIRIN", "08564666899", "Bojonegoro"}};
+        String data[][] = {{"ABDUL", "085646668991", "Kediri"}, {"KUSNO", "085646668992", "Trenggalek"}, {"PONIRIN", "085646668999", "Bojonegoro"}};
         //menampilkan judul tabel
         System.out.println("NAMA       ALAMAT        TELEPON");
         System.out.println("--------------------------------------");
@@ -10,6 +10,13 @@ public class Percobaan2 {
             String nama = data[a][0];       //menyimpan nama 
             String alamat = data[a][2];     //menyimpan alamat
             String telp = data [a][1];      //menyimpan nomor telepon
+            
+            //menampilkan nama dan spasi
+            System.out.print(nama);
+            for(int i = nama.length(); i < 10; i++){
+                System.out.print(" ");
+            }
+           
         }
     }
 }
