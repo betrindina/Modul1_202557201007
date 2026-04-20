259f1ec90c955a9e0d442449d529e9cb46056d68
betrindina <betrindina@gmail.com>
2026-04-20 19:12:09 +0700

    first commit

---
+++ src/percobaan1_java/Percobaan1_java.java
@@ -0,0 +1,23 @@
+package percobaan1_java;
+import javax.swing.JOptionPane;
+public class Percobaan1_java {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //mendeklarasikan variabel
+        int data[][] = {{4, 6, 4, 2, 8, 4, 2, 10}, {4, 6, 4, 2, 8, 4, 2, 10}};
+        int jumlah = 0;
+        int elemen = 0;
+        
+        System.out.println("=== DATA ARRAY ===");
+        for (int a = 0; a < data.length; a++) {
+            for (int b = 0; b < data[0].length; b++) {
+                System.out.print(data[a][b] + " ");
+                jumlah += data[a][b];
+                elemen++;
+            }
+            System.out.println();
+        }
+     
+    }
+    
+}
