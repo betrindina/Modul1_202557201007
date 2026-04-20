85a75e2d017afb4187f04be5b6d9bc4c53544427
betrindina <betrindina@gmail.com>
2026-04-20 22:54:56 +0700

    commit awal

---
+++ src/Percobaan2.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Percobaan2 {
+    public static void main(String[] args) {
+        String data[][] = {{"ABDUL", "085646668991", "Kediri"}, {"KUSNO", "085646668992", "Trenggalek"}, {"PONIRIN", "08564666899", "Bojonegoro"}};
+    }
+}
